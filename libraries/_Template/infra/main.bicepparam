57c62de4e322cc63ff11a77166484535eebd9e16
dae-ne <rafalcz29@gmail.com>
2025-11-17 17:08:36 +0100

    Add integration project template

---
+++ libraries/_Template/infra/main.bicepparam
@@ -0,0 +1,9 @@
+using './main.bicep'
+
+param projectName = 'int'
+param integrationName = toLower('Template')
+param sharedStorageAccountName = 'stintsharedg7lv'
+param sharedKeyVaultName = 'kv-int-shared-k4h7'
+param keyValultSecretName = 'Template-Secret'
+param timeZone = 'Central European Standard Time'
+param subtaskLabelsCheckSchedule = '0 0 2 * * *'
---
+++ libraries/_Template/src/Functions/TimerTriggerFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Integrations.Template.Functions;
+
+internal sealed class TimerTriggerFunction(ILogger<TimerTriggerFunction> logger)
+{
+    [Function(nameof(TimerTriggerFunction))]
+    public void Run([TimerTrigger("%TimerTriggerSchedule%", UseMonitor = false)] TimerInfo timer)
+    {
+        logger.LogInformation("C# Timer trigger function executed at: {DateTime}", DateTime.Now);
+
+        if (timer.ScheduleStatus is not null)
+        {
+            logger.LogInformation("Next timer schedule at: {ScheduleStatusNext}", timer.ScheduleStatus.Next);
+        }
+    }
+}
